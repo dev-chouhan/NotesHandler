40e9aeb6aba95e2cb7892463141df57f05b833f2
Dev Chouhan <devchouhan24git@gmail.com>
2023-02-08 19:42:16 +0530

    able to delete and update notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -26,7 +26,7 @@ router.post(
     async (req, res) => {
         try {
             const { title, description, tag } = req.body;
-            // If there are errors, return bad request and the error
+            // If there are errors, return bad request and the error. Here errors will be for title and description validation only.
             const errors = validationResult(req); // errors will be a array
             if (!errors.isEmpty()) {
                 return res.status(400).json({ errors: errors.array() });
@@ -46,26 +46,52 @@ router.post(
     }
 );
 
-// Route 3: update an existing note. Login required
+// Route 3: update an existing note using PUT. Login required
 router.put("/updateNode/:id", fetchUser, async(req, res)=>{
-    const {title, description, tag} = req.body;
-    // Create new note object
-    const newNote = {};
-    if(title){newNote.title = title};
-    if(description){newNote.description = description};
-    if(tag){newNote.tag = tag};
-
-    // find note to be updated
-    let note = await Note.findById(req.params.id);
-    // if someone else trying to fetch someone's data
-    if(!note){return res.status(404).send("Not Found")};
-    // if current user trying to fetch other user data
-    if(note.user.toString() !== req.user.id){
-        return res.status(401).send("Not Allowed");
+    try{
+        const {title, description, tag} = req.body;
+        // Create new note object
+        const newNote = {};
+        if(title){newNote.title = title};
+        if(description){newNote.description = description};
+        if(tag){newNote.tag = tag};
+    
+        // find note to be updated
+        // here id params.id defines the header inputed id in '/updateNode/:id'
+        let note = await Note.findById(req.params.id);
+        // if someone else trying to fetch someone's data
+        if(!note){return res.status(404).send("Not Found")};
+        // if current user trying to fetch other user data
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send("Not Allowed");
+        }
+    
+        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+        res.json({note});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error occured");
     }
+})
 
-    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
-    res.json({note});
+// Route 4: Delete an existing note using DELETE. Login required
+router.delete("/deleteNote/:id", fetchUser, async(req, res)=>{
+    try{
+        // here id params.id defines the header inputed id in '/deleteNote/:id'
+        let note = await Note.findById(req.params.id);
+        // if someone else trying to fetch someone's data
+        if(!note){return res.status(404).send("Not Found")};
+        // allow deletion only if user owns this note 
+        if(note.user.toString() !== req.user.id){
+            return res.status(401).send("Not Allowed");
+        }
+    
+        note = await Note.findByIdAndDelete(req.params.id);
+        res.json({"Success":"Note has been deleted", note: note});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error occured");
+    }
 })
 
 module.exports = router;
